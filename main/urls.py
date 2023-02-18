71238eac5566d59923c487e31220fc151a0dee9f
teki45 <goteki@protonmail.com>
2023-02-18 15:32:57 +0000

    Fix sign up and in paths having the same name

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("signup/", SignUp.as_view(), name="signup"),
-    path("signin/", SignIn.as_view(), name="signup"),
+    path("signin/", SignIn.as_view(), name="signin"),
 ]
