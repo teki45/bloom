5d0ccb9bc5f549f0b426767dedd434471fc843a5
teki45 <goteki@protonmail.com>
2023-02-18 03:08:00 +0000

    Touch up styles, scaffold sign up/in pages

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
+    path("signup/", SignUp.as_view(), name="signup"),
+    path("signin/", SignIn.as_view(), name="signup"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,12 @@ from django.views import View
 class Home(View):
     def get(self, request):
         return render(request, "home.html")
+
+class SignUp(View):
+    def get(self, request):
+        return render(request, "auth/signup.html")
+
+class SignIn(View):
+    def get(self, request):
+        return render(request, "auth/signin.html")
+
