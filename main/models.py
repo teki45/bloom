1ebaf66e26425d3a22c37260b2b89a5a9fa34584
teki45 <goteki@protonmail.com>
2023-02-21 23:37:23 +0000

    Create video model and remove "css" field from user model

--- main/models.py
+++ main/models.py
@@ -3,4 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     nickname = models.CharField(max_length=64, blank=True, null=True)
-    bio = models.TextField(max_length=1000, blank=True, null=True)
+    bio = models.TextField(max_length=1024, blank=True, null=True)  
+
+class Video(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=1024)
+    temp_file = models.FileField(upload_to='temp/videos/')
+    vid = models.CharField(max_length=8)
+    video_file = models.CharField(max_length=1024)
