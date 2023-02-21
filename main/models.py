6cee706aa41b76683ee3d060cca5abacf0eef0a7
teki45 <goteki@protonmail.com>
2023-02-21 23:23:31 +0000

    Extend user model and set as default model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    nickname = models.CharField(max_length=64, blank=True, null=True)
+    bio = models.TextField(max_length=1000, blank=True, null=True)
